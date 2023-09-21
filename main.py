c4741f1a94a0fecf585b7500e687459d01b46b70
marusaP <marusa.5rovcic@gmail.com>
2023-09-21 11:33:39 +0200

    krneki

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
-    print("moje besedilo")
+#! /usr/bin/python2
 
-if __name__ == "__main__":
-    main ()
+# za izvajanje pozeni  chmod +x main.py 
+#./main.py
+import time         #dodan modul za nadzor casa v programu
+"""#######################################################
+Primer dodajanja modulov 
+##########################################################"""
+#from time import sleep
+#from time import *              #importaj vse
+#import time as t    # preimenujemo modul
+
+def main():     # dodamo funkcijo main za omejiev scope
+    
+    print("moje besedilo")      #primer print stavka
+    print("moje stevilo: %d" % 5)  #primer formatranja stevil v stringu (print formats)
+    time.sleep(1.54)        # primer cakanja 1.54 sekunde
+
+if __name__ == "__main__":          # preverimo ce je bil program zagnan kot modul ali kot program 
+    main ()                     # klicemo funkcijo main 
